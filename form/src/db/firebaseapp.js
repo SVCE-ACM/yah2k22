ee28477ec7b287dd3cf7b201ff605f8943f025f6
Poujhit <poujhit.dev@gmail.com>
2022-05-15 00:39:48 +0530

    using env variable now

--- form/src/db/firebaseapp.js
+++ form/src/db/firebaseapp.js
@@ -2,19 +2,16 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore/lite';
 // import { getAnalytics } from 'firebase/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAxyVq6rHenKY6dYg1NzKNCs6fW3UBvTjE',
-  authDomain: 'yah2k22.firebaseapp.com',
-  projectId: 'yah2k22',
-  storageBucket: 'yah2k22.appspot.com',
-  messagingSenderId: '829518290113',
-  appId: '1:829518290113:web:31c94f160684e4c01dc840',
-  measurementId: 'G-H8EH4J7NQT',
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
 
 // Initialize Firebase
