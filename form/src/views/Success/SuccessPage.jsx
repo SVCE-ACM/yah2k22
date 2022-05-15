4a90effa71e5f3143e076c5050fa0da3e0bc15c7
Poujhit <poujhit.dev@gmail.com>
2022-05-15 11:03:05 +0530

    Success page added

--- form/src/views/Success/SuccessPage.jsx
+++ form/src/views/Success/SuccessPage.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
-const SuccessPage = (props) => {
-  return <div>done</div>;
+const SuccessPage = () => {
+  return (
+    <h3
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      Done
+    </h3>
+  );
 };
 export default SuccessPage;
