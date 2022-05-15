737f773148b043a85510cab62a97da2cc4be46c9
Poujhit <poujhit.dev@gmail.com>
2022-05-15 21:44:05 +0530

    Success page done

--- form/src/views/Success/SuccessPage.jsx
+++ form/src/views/Success/SuccessPage.jsx
@@ -1,18 +1,50 @@
 import React from 'react';
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material';
+// import DoneIllustration from 'images/done.svg';
 
 const SuccessPage = () => {
+  const theme = useTheme();
+  const match = useMediaQuery(theme.breakpoints.down('md'));
   return (
-    <h3
-      style={{
+    <Box
+      sx={{
         display: 'flex',
-        justifyContent: 'center',
         alignItems: 'center',
-        width: '100vw',
-        height: '100vh',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        overflow: 'hidden',
+        overflowY: 'hidden',
       }}
     >
-      Done
-    </h3>
+      <Box
+        component='img'
+        src={'/done.svg'}
+        sx={{
+          height: match ? '60vh' : '70vh',
+          width: match ? '60vw' : '70vw',
+        }}
+      />
+      <Typography
+        variant={match ? 'subtitle1' : 'h6'}
+        sx={{ fontWeight: 'bold' }}
+      >
+        You have successfully completed your registration.
+      </Typography>
+      <Typography
+        variant={match ? 'subtitle1' : 'h6'}
+        sx={{ fontWeight: 'bold' }}
+      >
+        Stay tuned for the results. For regular updates, follow our{' '}
+        <a
+          href='https://www.instagram.com/svceacm/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Instagram
+        </a>{' '}
+        page.
+      </Typography>
+    </Box>
   );
 };
 export default SuccessPage;
